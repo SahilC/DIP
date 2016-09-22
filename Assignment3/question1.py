4afa09ad33ef93db3dd1ee75688b477e217fc280
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-22 14:36:53 +0530

    Added various masks to extract pitch

--- Assignment3/question1.py
+++ Assignment3/question1.py
@@ -1,4 +1,12 @@
 import cv2
 import numpy as np
 
-im = cv2.imread('soccer_1.png')
+for i in xrange(1,11):
+    im = cv2.imread('soccer_'+str(i)+'.png')
+    hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([38,16,25])
+    upper_green = np.array([70,200,200])
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_and(im,im, mask= mask)
+    cv2.imshow('soccer_'+str(i)+'.png',res)
+    cv2.waitKey(0)
