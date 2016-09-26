510cbb209cdc1a01bab6698114d1189b358dd286
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-26 12:49:50 +0530

    Modifications

--- Assignment3/question1.py
+++ Assignment3/question1.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 #
-element_dilate = cv2.getStructuringElement(cv2.MORPH_RECT,( 50,50 ),( 0, 0))
-element_close = cv2.getStructuringElement(cv2.MORPH_RECT,( 50,50 ),( 0, 0))
+element_dilate = cv2.getStructuringElement(cv2.MORPH_RECT,( 7,7 ),( 0, 0))
+element_close = cv2.getStructuringElement(cv2.MORPH_RECT,( 7,7 ),( 0, 0))
 # im = cv2.imread('soccer_1.png')
 # cv2.imshow("XYZZZ",im[len(im)-100:len(im),1:100])
 
@@ -20,7 +20,7 @@ for i in xrange(1,11):
     mask = cv2.medianBlur(mask,3)
 
     #Finding connected components of Above
-    retval, thres = cv2.threshold(mask, 150, 255, cv2.THRESH_OTSU)
+    retval, thres = cv2.threshold(mask.copy(), 150, 255, cv2.THRESH_OTSU)
     retval, labels = cv2.connectedComponents(thres)
     output = np.zeros_like(labels, dtype=np.uint8)
     cv2.normalize(labels, output, 0 , 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
@@ -33,15 +33,21 @@ for i in xrange(1,11):
 
     #Selecting Area not on the Ground
     output = cv2.bitwise_not(output)
-    #output = cv2.erode(cv2.dilate(output,element_dilate),element_close)
-    ret,image_thresh = cv2.threshold(output, 125, 255, cv2.THRESH_BINARY)
+    # output = cv2.erode(cv2.dilate(output,element_dilate),element_close)
+
+    ret,image_thresh = cv2.threshold(output.copy(), 125, 255, cv2.THRESH_OTSU)
     _,contour,_ = cv2.findContours(image_thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     #output = cv2.drawContours(output.copy(),contour,0,150,cv2.FILLED)
 
     #Eliminating Area of Crowd :- Largest Contour of the inverted Area
     img = np.zeros( (im.shape[0],im.shape[1]))
-    cv2.drawContours(img, contour[:-2], -1,255,-1)
-
+    # cv2.drawContours(img, contour[:-2], -1,255,-1)
+    for cnt in contour[:-2]:
+        x,y,w,h = cv2.boundingRect(cnt)
+        area = cv2.contourArea(cnt)
+        # if h > w:
+        if h*1.0/w > 1 and h*1.0/w < 5:
+            cv2.drawContours(img, [cnt], -1,255,-1)
     #print cv2.convexHull(output)
     # im = cv2.blur(im,(3,3))
     # gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -51,6 +57,7 @@ for i in xrange(1,11):
     # cv2.drawContours(temp, cnt, -1, 255,cv2.FILLED)
     #
     # _,temp = cv2.threshold(temp, 128, 255, cv2.THRESH_BINARY)
-    #mask = cv2.bitwise_and(im,im,mask=mask)
+    # mask = cv2.bitwise_and(im,im,mask=output)
+    cv2.imshow('soccer_orig'+str(i)+'.png',im)
     cv2.imshow('soccer_'+str(i)+'.png',img)
     cv2.waitKey(0)
